eec7d7f70b9d0a5cf06aad32c29c845d3c18836a
Aswath K <aswath.sana@gmail.com>
2019-04-28 22:21:29 +0530

    Adds s3head utility script

---
+++ util_scripts/s3head.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function s3head() {
+    NUM_LINES=10
+    while test $# -gt 0; do
+        case $1 in 
+            -h|--help)
+                echo "s3head [-n NUM] <S3_FILE_PATH>"
+                return
+                ;;
+            -n)
+                shift
+                if test $# -gt 0; then
+                    export NUM_LINES=$1
+                else
+                    echo "Number of lines not specified"
+                    return
+                fi
+                shift
+                ;;
+            *)
+                break
+                ;;
+        esac
+    done
+    if [[ -z "$1" ]]; then
+        echo "S3 file path is not specified"
+        return
+    fi
+    s3cmd get "$1" - | gzip -d - | head -n "$NUM_LINES"
+}
