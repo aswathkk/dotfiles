114022494de638ae50999734624b0f256d2c1c79
Aswath K <aswath.sana@gmail.com>
2019-12-13 22:33:50 +0530

    update s3 alias for better usability

--- util_scripts/s3utils.sh
+++ util_scripts/s3utils.sh
@@ -1,41 +1,21 @@
 #!/bin/bash
 
-function shead() {
-    NUM_LINES=10
-    while test $# -gt 0; do
-        case $1 in 
-            -h|--help)
-                echo "s3head [-n NUM] <S3_FILE_PATH>"
-                return
-                ;;
-            -n)
-                shift
-                if test $# -gt 0; then
-                    export NUM_LINES=$1
-                else
-                    echo "Number of lines not specified"
-                    return
-                fi
-                shift
-                ;;
-            *)
-                break
-                ;;
-        esac
-    done
-    if [[ -z "$1" ]]; then
-        echo "S3 file path is not specified"
-        return
-    fi
-    s3cmd get "$1" - | zcat -f | head -n "$NUM_LINES"
+function shead {                                                                                                                               
+    s3_path=${@:$#}
+    params=${@:1:$# - 1}
+    s3cmd get $s3_path - | zcat -f | head $params
 }
 
 function scat {
-    s3cmd get "$1" - | zcat -f
+    s3_path=${@:$#}
+    params=${@:1:$# - 1}
+    s3cmd get $s3_path - | zcat -f | cat $params
 }
 
 function sless {
-    s3cmd get "$1" - | zcat -f | less
+    s3_path=${@:$#}
+    params=${@:1:$# - 1}
+    s3cmd get $s3_path - | zcat -f | less $params
 }
 
 function ssize {
