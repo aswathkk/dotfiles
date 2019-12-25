5a120ced26802872d76c082f83142767a08f6fd9
Aswath K <aswath.sana@gmail.com>
2019-12-25 23:54:09 +0530

    Adds copy buffer alias

--- util_scripts/bash_utils.sh
+++ util_scripts/bash_utils.sh
@@ -4,6 +4,16 @@ function mcd() {
     mkdir -p "$1" && cd "$1"
 }
 
+# Copy from remote tmux buffer to local clipboard
+function cpb {
+    ssh "$1" "tmux saveb -" | xclip -sel c
+}
+
+# Copy from remote tmux buffer to local tmux clipboard
+function cpt {
+    ssh "$1" "tmux saveb -" | tmux loadb -
+}
+
 # Alias
 alias tmux='tmux -u -f "$XDG_CONFIG_HOME"/tmux/.tmux.conf'
 alias whatsmyip='curl http://ipinfo.io/ip'
