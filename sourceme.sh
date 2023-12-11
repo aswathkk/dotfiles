f08d9e81df61af54f7b98620a2295c6c21d4e802
Aswath K <aswath.sana@gmail.com>
2023-12-11 10:42:43 +0530

    chore: Removes unused files and configs

--- sourceme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export ASWATHKK_DOTFILES=$(cd "$(dirname "$0")"; pwd)
-
-source $ASWATHKK_DOTFILES/zsh/.zshrc
--- util_scripts/refresh_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-function refresh_config {
-    cd $XDG_CONFIG_HOME
-    git pull origin master
-    source sourceme.sh
-    cd -
-}
